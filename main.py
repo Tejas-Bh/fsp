897d09413137cd59c3e510706ebad7bb65bd9018
Tejas Bhagawatula <tejas.bhagawatula@gmailcom>
2024-05-26 14:15:59 -0500

    error handling by using error_template parameter in config.toml

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ This program also uses markdown, meaning that it's really easy to make sites.
 with open("config.toml", mode="rb") as config:
     fsp = tomli.load(config)
 
+error_template = True
+try:
+    fsp["error_template"]
+except:
+    error_template = False
+
 # Do the flask stuff
 from flask import Flask
 
@@ -23,7 +29,7 @@ app = Flask(__name__)
 
 # using dynamic routes to go through all html in templates/ and make routes out of them
 
-print()
+print(f"templates/{fsp['error_template']}")
 
 # Make index route
 md = mkdown.Markdown(extensions=["meta"])
@@ -39,34 +45,55 @@ def index():
     with open("templates/base.html") as f:
         for l in f:
             base += l
+    if error_template:
+        err = """"""
+        with open(f"templates/{fsp['error_template']}") as f:
+            for l in f:
+                err += l
 
     html_body = md.convert(md_text)
 
     try:
         return jinja_env.from_string(base).render(body=html_body, md_metadata=md.Meta, md=md, fsp=fsp)
     except Exception as e:
-        return f"There was an error: {e}"
+        if error_template:
+            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp)
+        else:
+            return f"ERROR: {e}"
 
 # Make dynamic route for all pages
 md = mkdown.Markdown(extensions=["meta"])
 @app.route("/<webpage>")
 def pages(webpage):
-    md_text = """"""
-    with open(f"content/{webpage}.md") as f:
-        for l in f:
-            md_text += l
-
     base = """"""
     with open("templates/base.html") as f:
         for l in f:
             base += l
+    if error_template:
+        err=""""""
+        with open(f"templates/{fsp['error_template']}") as f:
+            for l in f:
+                err += l
+    md_text = """"""
+    try:
+        with open(f"content/{webpage}.md") as f:
+            for l in f:
+                md_text += l
+    except Exception as e:
+        if error_template:
+            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp)
+        else:
+            return f"ERROR: {e}"
 
     html_body = md.convert(md_text)
  
     try:
         return jinja_env.from_string(base).render(body=html_body, md_metadata=md.Meta, md=md, fsp=fsp)
     except Exception as e:
-        return f"There was an error: {e}"
+        if error_template:
+            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp)
+        else:
+            return f"ERROR: {e}"
 
 if __name__ == "__main__":
     app.run()
