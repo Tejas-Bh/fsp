b40d23cc5dfe9f151f28ba384229dd19e4acd0a1
Tejas Bhagawatula <tejas.bhagawatula@gmailcom>
2024-05-27 23:08:01 -0500

    custom scripts! basically it allows you to do your own python inside of your websites it's pretty cool will add documentation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import markdown as mkdown
 import jinja2
 import tomli
 from os import chdir, getcwd
+from sys import path
 
 ######################################
 """
@@ -25,6 +26,27 @@ try:
 except:
     error_template = False
 
+scripts_usage = True
+try:
+    fsp["scripts_usage"]
+except:
+    scripts_usage = False
+if scripts_usage:
+    if fsp["scripts_usage"]:
+        try:
+            path.insert(1, getcwd())
+            import scripts
+        except Exception as e:
+            raise Exception(f"""
+It looks like there's an error. Try:
+If you have scripts_usage in config.toml set to true, make sure that scripts.py exists.
+Else, if you don't mean to use outside scripts, don't use scripts_usage in config.toml.
+Hope this helps!
+-fsp
+Meanwhile, here's the original error message:
+{e}""") from None
+
+
 # Do the flask stuff
 from flask import Flask, url_for
 
@@ -35,8 +57,6 @@ def static(file):
     return url_for("static", filename=file)
 
 # using dynamic routes to go through all html in templates/ and make routes out of them
-
-print(f"templates/{fsp['error_template']}")
 
 # Make index route
 md = mkdown.Markdown(extensions=["meta"])
@@ -58,14 +78,14 @@ def index():
             for l in f:
                 err += l
 
-    md_text = jinja_env.from_string(md_text).render(fsp=fsp, static=static)
+    md_text = jinja_env.from_string(md_text).render(fsp=fsp, static=static, scripts=scripts)
     html_body = md.convert(md_text)
 
     try:
-        return jinja_env.from_string(base).render(body=html_body, md_metadata=md.Meta, md=md, fsp=fsp)
+        return jinja_env.from_string(base).render(body=html_body, md_metadata=md.Meta, md=md, fsp=fsp, scripts=scripts)
     except Exception as e:
         if error_template:
-            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp)
+            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp, scripts=scripts)
         else:
             return f"ERROR: {e}"
 
@@ -89,18 +109,18 @@ def pages(webpage):
                 md_text += l
     except Exception as e:
         if error_template:
-            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp)
+            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp, scripts=scripts)
         else:
             return f"ERROR: {e}"
 
-    md_text = jinja_env.from_string(md_text).render(fsp=fsp, static=static)
+    md_text = jinja_env.from_string(md_text).render(fsp=fsp, static=static, scripts=scripts)
     html_body = md.convert(md_text)
  
     try:
-        return jinja_env.from_string(base).render(body=html_body, md_metadata=md.Meta, md=md, fsp=fsp)
+        return jinja_env.from_string(base).render(body=html_body, md_metadata=md.Meta, md=md, fsp=fsp, scripts=scripts)
     except Exception as e:
         if error_template:
-            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp)
+            return jinja_env.from_string(err).render(body=e, md_metadata=md.Meta, md=md, fsp=fsp, scripts=scripts)
         else:
             return f"ERROR: {e}"
 
